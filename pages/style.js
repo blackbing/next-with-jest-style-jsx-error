718d2e43b0aca114a0c633f561f65d22931da482
bingo <blackbing@gmail.com>
2018-05-31 22:19:32 +0800

    narrow down the problem, remove unnecessary packages
    https://github.com/zeit/next.js/issues/4265#issuecomment-393494504

--- pages/style.js
+++ pages/style.js
@@ -1,20 +1,5 @@
 import css from 'styled-jsx/css';
 
-const color = 'red';
-const backgroundColor = '#eee';
-
 export default css`
-    .hello {
-        background-color: ${backgroundColor};
-        padding: 100px;
-        text-align: center;
-        transition: 100ms ease-in background;
-
-        &:hover {
-            color: ${color};
-        }
-        @media only screen and (max-width: 480px) {
-            font-size: 20px;
-        }
-    }
+    div { color: red }
 `;
